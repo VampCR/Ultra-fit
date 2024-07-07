6d0f3754381ebc9e2b67296a0eccb5ad02d45b67
VampCR <rahatechinmay1@gmail.com>
2024-07-07 17:57:06 +0530

    Add files via upload

--- Ultra Fit/script.js
+++ Ultra Fit/script.js
@@ -101,3 +101,76 @@ document.addEventListener("DOMContentLoaded", function () {
     form.reset();
   }
 });
+
+// pro and cart---------------------------------------------------
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart() {
+  const productName = document.querySelector(".product-name").innerText;
+  const productPrice = parseFloat(
+    document.querySelector(".product-price").innerText.replace("$", "")
+  );
+  const productQuantity = parseInt(document.getElementById("quantity").value);
+  const productImage = document.querySelector(".product-image img").src;
+
+  const product = {
+    name: productName,
+    price: productPrice,
+    quantity: productQuantity,
+    image: productImage,
+  };
+
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Product added to cart!");
+}
+
+function displayCart() {
+  const cartItemsContainer = document.getElementById("cart-items");
+  const totalAmountContainer = document.getElementById("total-amount");
+  let totalAmount = 0;
+
+  cartItemsContainer.innerHTML = "";
+
+  cart.forEach((item, index) => {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+
+    cartItem.innerHTML = `
+            <img src="${item.image}" alt="Product Image">
+            <div>
+                <p>${item.name}</p>
+                <p>$${item.price}</p>
+                <div class="update-quantity">
+                    <label for="quantity-${index}">Quantity:</label>
+                    <input type="number" id="quantity-${index}" name="quantity" min="1" value="${item.quantity}" onchange="updateQuantity(${index}, this.value)">
+                </div>
+            </div>
+            <div class="button-c">
+                <button class="remove-btn" onclick="removeFromCart(${index})"><svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg></button>
+            </div> 
+            
+        `;
+
+    totalAmount += item.price * item.quantity;
+    cartItemsContainer.appendChild(cartItem);
+  });
+
+  totalAmountContainer.innerText = totalAmount.toFixed(2);
+}
+
+function updateQuantity(index, quantity) {
+  cart[index].quantity = parseInt(quantity);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
+if (window.location.pathname.includes("cart.html")) {
+  displayCart();
+}
